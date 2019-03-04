cff588d7f74aab40331245e6fd3f2579df1689a7
Dudka <37344843+Dorian77210@users.noreply.github.com>
2019-03-04 13:26:52 +0100

    Update check-auth.js

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -2,9 +2,15 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     try {
-        const token = req.headers.authorization.split(' ')[1];
+        //const token = req.headers.authorization.split(' ')[1];
+        const token = req.body.token || req.query.token || req.headers['x-access-token'] || window.localStorage.getItem('token');
         const decoded = jwt.verify(token, process.env.JWT_KEY);
         req.userData = decoded;
+        //store the token in the local storage
+        //if(!window.localStorage.getItem('token')) {
+          //  window.localStorage.setItem('token', token);
+        //}
+        
         next();
     } catch(error) {
         return res.status(401).json({
